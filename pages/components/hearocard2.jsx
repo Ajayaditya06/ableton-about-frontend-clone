86b0335752a9988d9649d52156f2faba5b4399f6
Ajayaditya06 <133016547+Ajayaditya06@users.noreply.github.com>
2023-07-27 12:14:33 +0530

    Update hearocard2.jsx

--- pages/components/hearocard2.jsx
+++ pages/components/hearocard2.jsx
@@ -4,7 +4,7 @@ export default function HeroCard2() {
   return (
     <div className="w-[100%] flex justify-center items-center">
       <Image
-        src="/poster-meet-the-makers.avif"
+        src="/poster-meet-the-makers.png"
         height="700"
         width="700"
         style={{ height: "90vh", width: "80vw" }}
